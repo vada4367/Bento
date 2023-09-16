b3f913f480ede3eb07353b8df3c44e4b0920987c
vada4367 <88112844+vada4367@users.noreply.github.com>
2023-09-16 17:13:15 +0300

    Update config.js

--- config.js
+++ config.js
@@ -77,9 +77,9 @@ const CONFIG = {
 		},
 		{
 			id: '5',
-			name: 'Reddit',
-			icon: 'glasses',
-			link: 'https://reddit.com',
+			name: 'Telegram',
+			icon: 'telegram',
+			link: 'https://web.telegram.org',
 		},
 		{
 			id: '6',
